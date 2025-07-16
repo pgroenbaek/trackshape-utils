a77152c5d5b3db75c58d52d19f6c16d7277b8800
Peter Grønbæk Andersen <peter@grnbk.io>
2025-07-16 21:07:54 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="trackshapeutils",
-    version="0.4.0",
+    version="0.4.0b0",
     packages=find_packages(),
     install_requires=[
         'numpy',
