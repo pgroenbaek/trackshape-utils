3bb8ea2a4752db77f8d1ef46f32a86ddf46f5c9c
Peter Grønbæk Andersen <peter@grnbk.io>
2025-07-16 21:15:45 +0200

    Update version number

--- trackshapeutils/__init__.py
+++ trackshapeutils/__init__.py
@@ -37,7 +37,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
 
-__version__ = '0.4.0'
+__version__ = '0.4.0b0'
 __all__ = [
     'find_directory_files', 'load_file', 'load_shape',
     'generate_empty_centerpoints', 'generate_straight_centerpoints', 'generate_curve_centerpoints',
