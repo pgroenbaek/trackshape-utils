90760724e4bd5c883a1ba793a6e1e4b3b84424b0
Peter Grønbæk Andersen <peter@grnbk.io>
2025-12-07 21:20:52 +0100

    chore: exclude python3.6

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 )
